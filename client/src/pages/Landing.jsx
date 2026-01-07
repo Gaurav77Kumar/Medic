41b18e7a15779a6e90a5daf29d3ccc447dbff165
Gaurav <gauravkumar19114@gmail.com>
2026-01-07 15:29:08 +0530

    landing page

---
+++ client/src/pages/Landing.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import Navbar from '../components/Navbar.jsx'
+import HeroSection from '../components/HeroSection.jsx'
+import Footer from '../components/Footer.jsx'
+
+const Landing = () => {
+  return (
+    <>
+      <Navbar />
+      <HeroSection />
+      <Footer />
+
+
+     
+
+   
+
+    </>
+  )
+}
+
+export default Landing
